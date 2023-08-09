deec60fc327f658dce4bceedde33f0293557b19b
ayush-9453 <agrawal9453@gmail.com>
2023-08-09 21:53:52 +0530

    commit

--- slot machine/project.js
+++ slot machine/project.js
@@ -31,7 +31,7 @@ const deposit = () => {
 
 const getNumberOflines = () => {
   while (true) {
-    const lines = prompt("Enter the amount you want to bet on (1-3) : ");
+    const lines = prompt("Enter the lines you want to bet on (1-3) : ");
     const numberOfLines = parseFloat(lines);
 
     if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
@@ -44,7 +44,7 @@ const getNumberOflines = () => {
 
 const getBet = (balance, lines) => {
   while (true) {
-    const bet = prompt("Enter the total bet amount  : ");
+    const bet = prompt("Enter the total bet amount per lines : ");
     const numberbet = parseFloat(bet);
 
     if (isNaN(numberbet) || numberbet <= 0||numberbet > balance/ lines) {
@@ -74,8 +74,51 @@ const spin=()=>{
     }
     return reels;
 }
-const reels = spin();
-console.log(reels);
+const transpose = (reels)=>{
+  const rows =[];
+  for (let i=0 ; i<Rows; i++){
+    rows.push([]);
+    for(let j=0 ; j< Cols; j++){
+      rows[i].push(reels[j][i])
+    }
+  }
+  return rows;
+}
+const printRows = (rows) =>{
+  for(const row of rows){
+    let rowString = " ";
+    for(const [ i, symbol] of row.entries()){
+      rowString += symbol
+      if (i != row.length -1){
+        rowString += "|"
+      }
+    }
+    console.log(rowString)
+  }
+}
+const getwinnings =(rows, bet , lines)=>{
+  let winnings = 0;
+  for(let row =0; row<lines; row++){
+    const symbols = rows[row];
+    let allsame = true;
+      for(const symbol of symbols){
+        if (symbol!= symbols[0]){
+          allsame = false;
+          break;
+        }
+      }
+      if (allsame){
+        winnings +=bet *SYMBOL_VALUES[symbols[0]]
+      }
+  }
+  return winnings;
+}
+
 const balance = deposit();
 const numberOfLines = getNumberOflines();
-const numberbet = getBet(balance, numberOfLines);
+const bet = getBet(balance, numberOfLines);
+const reels = spin();
+const rows = transpose(reels);
+printRows(rows);
+const winnings = getwinnings(rows, bet , numberOfLines)
+console.log("You won,$" + winnings.toString());
