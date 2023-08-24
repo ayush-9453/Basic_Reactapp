4991d0aa6bb25e16107a8522e4da61f4cf136c4b
ayush-9453 <agrawal9453@gmail.com>
2023-08-24 22:10:42 +0530

    10

--- my-app/src/component/Navbar.js
+++ my-app/src/component/Navbar.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
-
+import kong from './kong4.png'
 export class Navbar extends Component {
     render() {
         return (
             <div>
                 <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="/">Navbar</a>
+                        <img src={kong} alt="kong" className='mx-2' />
+                        <a className="navbar-brand" href="/">News</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
@@ -20,24 +21,26 @@ export class Navbar extends Component {
                                 </li>
                                 <li className="nav-item dropdown">
                                     <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                      Catagories
+                                        Catagories
                                     </a>
                                     <ul className="dropdown-menu">
-                                        <li><a className="dropdown-item" href="/">General</a></li>
-                                        <li><hr className="dropdown-divider"/></li>
                                         <li><a className="dropdown-item" href="/">Bussiness</a></li>
-                                        <li><hr className="dropdown-divider"/></li>
+                                        <li><hr className="dropdown-divider" /></li>
                                         <li><a className="dropdown-item" href="/">Entertainment</a></li>
-                                        <li><hr className="dropdown-divider"/></li>
+                                        <li><hr className="dropdown-divider" /></li>
                                         <li><a className="dropdown-item" href="/">Sports</a></li>
-                                        <li><hr className="dropdown-divider"/></li>
+                                        <li><hr className="dropdown-divider" /></li>
                                         <li><a className="dropdown-item" href="/">Health</a></li>
-                                        <li><hr className="dropdown-divider"/></li>
+                                        <li><hr className="dropdown-divider" /></li>
                                         <li><a className="dropdown-item" href="/">Science</a></li>
 
                                     </ul>
                                 </li>
                             </ul>
+                            <form className="d-flex" role="search">
+                                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                                <button className="btn btn-outline-primary" type="submit">Search</button>
+                            </form>
                         </div>
                     </div>
                 </nav>
