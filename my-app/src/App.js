80de55ff73896d17b8fa649483dc66fddbdfe8f1
ayush-9453 <agrawal9453@gmail.com>
2023-08-24 23:20:52 +0530

    10

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,23 @@
 // adding a search box in the navbar 
 import './App.css';
-
 import React, { Component } from 'react'
 import Navbar from './component/Navbar';
 import News from './component/News';
-
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 export default class App extends Component {
   render() {
     return (
       <div>
-           <Navbar/>
-           <News pageSize={6} country="in" category='entertainment' />
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<News key="general" pageSize={6} country="us" category='general'/>}></Route>
+            <Route exact path="/bussiness" element={<News key="business" pageSize={6} country="in" category='business'/>}></Route>
+            <Route exact path="/entertainment" element={<News key="entertainment" pageSize={6} country="us" category='entertainment'/>}></Route>
+            <Route exact path="/sports" element={<News key="sports" pageSize={6} country="us" category='sports' />}></Route>
+            <Route exact path="/science" element={<News key="science" pageSize={6} country="in" category='science' />}></Route>
+          </Routes>
+        </Router>
       </div>
     )
   }
--- my-app/src/component/Navbar.js
+++ my-app/src/component/Navbar.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import kong from './kong4.png'
+import { Link } from "react-router-dom";
+
 export class Navbar extends Component {
     render() {
         return (
@@ -7,32 +9,32 @@ export class Navbar extends Component {
                 <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
                     <div className="container-fluid">
                         <img src={kong} alt="kong" className='mx-2' />
-                        <a className="navbar-brand" href="/">News</a>
+                        <Link className="navbar-brand" to="/">News</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="/">Home</a>
+                                    <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/">About</a>
+                                    <Link className="nav-link" to="/">About</Link>
                                 </li>
                                 <li className="nav-item dropdown">
-                                    <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <a className="nav-link dropdown-toggle" to="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                         Catagories
                                     </a>
                                     <ul className="dropdown-menu">
-                                        <li><a className="dropdown-item" href="/">Bussiness</a></li>
+                                        <li><Link className="dropdown-item" to="/business">Bussiness</Link></li>
                                         <li><hr className="dropdown-divider" /></li>
-                                        <li><a className="dropdown-item" href="/">Entertainment</a></li>
+                                        <li><Link className="dropdown-item" to="/entertainment">Entertainment</Link></li>
                                         <li><hr className="dropdown-divider" /></li>
-                                        <li><a className="dropdown-item" href="/">Sports</a></li>
+                                        <li><Link className="dropdown-item" to="/sports">Sports</Link></li>
                                         <li><hr className="dropdown-divider" /></li>
-                                        <li><a className="dropdown-item" href="/">Health</a></li>
+                                        <li><Link className="dropdown-item" to="/health">Health</Link></li>
                                         <li><hr className="dropdown-divider" /></li>
-                                        <li><a className="dropdown-item" href="/">Science</a></li>
+                                        <li><Link className="dropdown-item" to="/science">Science</Link></li>
 
                                     </ul>
                                 </li>
