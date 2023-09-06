e4c3b798a7b050b0a548d9f608ba47adf1fdf70a
ayush9453 <agrawal9453@gmail.com>
2023-09-06 18:09:02 +0530

    commit

---
+++ socialmedia/src/App.js
@@ -0,0 +1,13 @@
+import Home from "./pages/home/Home";
+import { Person } from "@mui/icons-material";
+
+function App() {
+  return (
+    <div>
+      <Home/>
+      <Person/>
+    </div>
+  );
+}
+
+export default App;
