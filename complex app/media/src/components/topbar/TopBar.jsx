baff0e65b541dabd2c92e6fc920182ac4337ebb4
ayush-9453 <agrawal9453@gmail.com>
2023-09-06 13:36:18 +0530

    social media

---
+++ complex app/media/src/components/topbar/TopBar.jsx
@@ -0,0 +1,13 @@
+
+import "./topbar.css"
+function TopBar() {
+  return (
+    <div className="topbarContainer">
+       <div className="topbarLeft"></div>
+       <div className="topbarCenter"></div>
+       <div className="topbarRight"></div>
+    </div>
+  )
+}
+
+export default TopBar
---
+++ complex app/media/src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TopBar from '../../components/topbar/TopBar'
+
+function Home() {
+  return (
+    <>
+      <TopBar/> 
+    </>
+  )
+}
+
+export default Home
