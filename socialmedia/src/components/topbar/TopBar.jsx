fa1367989c619efd76d6290ccbf0db2edd0128cf
ayush9453 <agrawal9453@gmail.com>
2023-09-07 21:36:21 +0530

    commit

--- socialmedia/src/components/topbar/TopBar.jsx
+++ socialmedia/src/components/topbar/TopBar.jsx
@@ -1,11 +1,38 @@
-
+import { Chat,Notifications, Search, Person} from "@mui/icons-material"
 import "./topbar.css"
 function TopBar() {
   return (
     <div className="topbarContainer">
-       <div className="topbarLeft"></div>
-       <div className="topbarCenter"></div>
-       <div className="topbarRight"></div>
+       <div className="topbarLeft">
+        <span className="logo">social</span>
+       </div>
+       <div className="topbarCenter">
+           <div className="serachbar">
+              <Search/>
+              <input type="text" placeholder="search for friend,post or video" className="searchInput" />
+           </div>
+       </div>
+       <div className="topbarRight">
+          <div className="topbarLinks">
+            <span className="topbarLink">Homepage</span>
+            <span className="topbarLink">Timeline</span>
+          </div>
+          <div className="topbarIcon">
+              <div className="topbarIconitem">
+                <Person/>
+                <span className="topbarIconBadge">1</span>
+              </div>
+              <div className="topbarIconitem">
+                <Chat/>
+                <span className="topbarIconBadge">1</span>
+              </div>
+              <div className="topbarIconitem">
+                <Notifications/>
+                <span className="topbarIconBadge">1</span>
+              </div>
+          </div>
+        <img src="./assets/person/1.jpg" alt="" className="topbarImg" />
+       </div>
     </div>
   )
 }
