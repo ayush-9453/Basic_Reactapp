748a4f5a7116afebf4d41c3149302e042da4490b
ayush-9453 <98285114+ayush-9453@users.noreply.github.com>
2023-09-10 20:47:59 +0530

    commit 10/9

--- socialmedia/src/components/rightbar/Rightbar.jsx
+++ socialmedia/src/components/rightbar/Rightbar.jsx
@@ -3,7 +3,23 @@ import React from 'react'
 const Rightbar = () => {
   return (
     <div className='rightbar'>
-      Rightbar
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img className="bithdayImg" src="assets/giftbox.png" alt="" />
+          <span className="birthdayText"><b>I U </b>and 3 other have there bithday today</span>       
+        </div>
+        <img src="assets/ad.png" className="rightbarAd" alt="" />
+        <h4 className="rightbarTitle">Online Friend</h4>
+        <ul className="rightbarFriendList">
+           <li className="rightBarFriend">
+              <div className="rightbarProfileImgContainer">
+                <img src="assets/person/3.jpg" className="rightbarProfileImg" alt="" />
+                <span className="rightOnline"></span>
+              </div>
+              <span className="rightbarUsername"></span>
+           </li>
+        </ul>
+      </div>
     </div>
   )
 }
