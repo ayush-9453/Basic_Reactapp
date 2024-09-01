d4b85c64f7103b14e84f2389bae29d8540efd0e4
Ayush agrawal <98285114+ayush-9453@users.noreply.github.com>
2024-09-01 18:14:15 +0530

    Update App.js

--- hospital/src/App.js
+++ hospital/src/App.js
@@ -3,22 +3,46 @@ import Sidebar from "./component/Sidebar";
 import Navbar from "./component/Navbar";
 import HospitalList from "./component/HospitalList";
 import Search from "./component/Search";
-import Footer from "./component/Footer"; 
+import Footer from "./component/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="bg-gradient-to-r from-customPurple via-customPurple1 to-pink-100 h-full ">
-      <Navbar />
-      <div className="flex flex-warp space-x-5 ">
-        <Sidebar />
-        <div className="w-screen flex flex-col">
-          <Search placeholder="Search for hospitals.." />
-          <div>
-            <HospitalList />
+      <Router>
+        <Navbar />
+        <div className="flex flex-warp space-x-5">
+          <Sidebar />
+          <div className="w-screen flex flex-col">
+            <Search placeholder="Search for hospitals.." />
+            <div>
+              <Routes>
+                <Route
+                  exact
+                  path="/hospital"
+                  element={<HospitalList page={'HOSPITAL'} />}
+                ></Route>
+                <Route
+                  exact
+                  path="/Radiologists"
+                  element={<HospitalList page={'RADIOLOGISTS'} />}
+                ></Route>
+                <Route
+                  exact
+                  path="/technicial"
+                  element={<HospitalList page={'TECHNICIAN'} />}
+                ></Route>
+                <Route
+                  exact
+                  path="/report"
+                  element={<HospitalList page={'REPORTS'}/>}
+                ></Route>
+              </Routes>
+            </div>
           </div>
         </div>
-      </div>
-      <Footer/>
+        <Footer />
+      </Router>
     </div>
   );
 }
